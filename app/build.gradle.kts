d0654517e7d7af9b3677920326c1e34e7c38511e
mbaiforinsticnt-dev <mbaiforinsticnt@gmail.com>
2026-09-19 22:29:03 +0100

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "dev.mbaiforinstinct.rebornlauncher"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "dev.mbaiforinstinct.rebornlauncher"
+        minSdk = 30
+        targetSdk = 30
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
