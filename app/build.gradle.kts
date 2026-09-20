7486ba9b3a742124ac36c44e86eda680b373d8de
mbaiforinsticnt-dev <mbaiforinsticnt@gmail.com>
2026-09-20 14:21:12 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
     defaultConfig {
         applicationId = "dev.mbaiforinstinct.rebornlauncher"
         minSdk = 30
-        targetSdk = 30
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
     }
