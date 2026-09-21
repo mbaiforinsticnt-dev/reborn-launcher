5af0d81926c5b52bf6de9c3cdc4243fc70411d53
mbaiforinsticnt-dev <mbaiforinsticnt@gmail.com>
2026-09-21 23:20:26 +0100

    Rename root project to RebornAPK [skip ci]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,5 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories { google(); mavenCentral() }
 }
-rootProject.name = "RebornLauncher"
+rootProject.name = "RebornAPK"
 include(":app")
